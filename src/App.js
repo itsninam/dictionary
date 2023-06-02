5b89a242e6ed802507cfb3f64f27637d9e10fe97
itsninam <madalinaoancea@hotmail.com>
2023-06-02 17:43:40 -0400

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
+import axios from "axios";
 
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/cat";
 const App = () => {
-  return <div className="App">Dictionary App</div>;
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  //fetch data
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+      setData(response.data[0]);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const { word, phonetic, meanings } = data;
+
+  return (
+    <div className="App">
+      <p>{word}</p>
+      <p>{phonetic}</p>
+    </div>
+  );
 };
 
 export default App;
